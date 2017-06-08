57c70e87504b90a3ff7404d2e98ffb4b5d1165ee
dayinfinte <zsq941556540@gmail.com>
2017-06-08 23:00:06 +0800

    create dns  file

--- controllers/dnsfilehandler.py
+++ controllers/dnsfilehandler.py
@@ -5,15 +5,28 @@
 import os
 import sys
 from ..service import db
+from jinja2 import Template, Environment, FileSystemLoader
 
+
+def templatExits(tfile):
+    if os.path.isfile(tfile):
+        return "{0} is exits".format(tfile)
 
 class DNSFileHandler:
 
     def __init__(self):
         pass
 
-    def CreateZoneFile(self, zone, cluster):
-        pass
+    def CreateZoneFile(self, zone, cluster, zone_record, domain_record):
+        env = Environment(loader=PackageLoader('service/templates/'))
+        template = env.get_template('db.zone_template')
+        temp = template.render(zone, zone_record, domain_record)
+        with open('db.{0}'.format(zone), 'w') as zonefile:
+            zonefile.write(temp)
 
-    def CreateNamedFile(self, zone, cluster):
-        pass
+    def CreateNamedFile(self, zones, type, ips=None):
+        env = Environment(loader=PackageLoader('service/templates/'))
+        template = env.get_template('named.conf')
+        temp = template.render(zone, type, ips)
+        with open('named.conf', 'w') as named_conf:
+            named_conf.write(temp)
